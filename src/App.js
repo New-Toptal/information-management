bcbf9a5fe91c7cecd6cacc8f66294c75680020c1
rcblue <rcbluekis@gmail.com>
2023-02-08 19:49:58 +1030

    welcome beautiful world

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Welcome Beautiful World
         </a>
       </header>
     </div>
